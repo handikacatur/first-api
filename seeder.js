0266f1fdda7b94e54b824010a338b82517ea4839
handikacatur <192410101066@students.unej.ac.id>
2020-07-27 10:38:30 +0700

    adding courses to seeder

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp.js');
+const Course = require('./models/Course.js')
 
 // Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,10 +22,15 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data imported successfully');
         process.exit();
     } catch (err) {
@@ -36,6 +42,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data destroyed successfully');
         process.exit();
     } catch (err) {
